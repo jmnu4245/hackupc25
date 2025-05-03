5deb54e0fbbe7f0689f31c3aaeba196c6191f102
Juan Manuel <juanmanuelfloresdelacruz@gmail.com>
2025-05-03 15:51:48 +0200

    arreglado

--- Front_Hack/src/App.jsx
+++ Front_Hack/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import Inditex_logo_black from '/Inditex_logo_black.svg'
 import './App.css'
 import ListaPrendas from './lista.jsx'
@@ -53,12 +53,10 @@ function App() {
         <a href="https://www.inditex.com" target="_blank">
           <img src={Inditex_logo_black} className="logo" alt="Inditex logo" />
         </a>
-
       <div>
         <h1>Catálogo de ropa</h1>
         <ListaPrendas />
       </div>
-
       </div>
       <h1>Encarni-chaaaan</h1>
       <div className="card">
